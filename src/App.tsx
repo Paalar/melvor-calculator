dc9074d0520b9d02027daaabf2d5662ce2680413
Paalar <paaledward@gmail.com>
2020-11-27 16:27:49 +0100

    add baseurl to project and add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { expTableAsLvls } from 'data/experienceTable';
+import Dropdown from 'react-dropdown';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropdown options={expTableAsLvls()} value={expTableAsLvls()[98]} />
     </div>
   );
 }
