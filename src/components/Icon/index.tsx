c098211d12c0d46401b54a30cd03dcbda28ef078
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-11-30 16:41:28 +0100

    unignore icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { SkillNamesType } from "../../common/skillNames";
+
+const Img = styled.img<{ size?: string }>`
+    width: ${props => props.size ? props.size : '32px'};
+    height: ${props => props.size ? props.size : '32px'};
+    border-style: none;
+    vertical-align: middle;
+`;
+
+const svgRoot = 'https://melvorcdn.fra1.cdn.digitaloceanspaces.com/current/assets/media/';
+const headerIconMain = `${svgRoot}/main`
+const combatIconMain = `${svgRoot}/skills/combat`;
+const iconMain = `${svgRoot}/skills`;
+
+const specialSources = {
+    'Combat': `${headerIconMain}/combat_header.svg`,
+    'Attack': `${combatIconMain}/attack.svg`,
+    'Strength': `${combatIconMain}/strength.svg`,
+    'Defence': `${combatIconMain}/defence.svg`,
+    'Hitpoints': `${combatIconMain}/hitpoints.svg`,
+}
+
+export const getSvgSrcFromPageName = (name: SkillNamesType) => {
+    if (name in specialSources) {
+        return specialSources[(name as keyof typeof specialSources)]
+    }
+    const pathName = name.toLowerCase();
+    return `${iconMain}/${pathName}/${pathName}.svg`;
+}
+
+const Icon: React.FC<{ pageName: SkillNamesType, size?: string }> = ({ pageName, size }) => {
+    return <Img src={getSvgSrcFromPageName(pageName)} size={size} />
+}
+
+export default Icon;
